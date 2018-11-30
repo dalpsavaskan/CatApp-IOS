814236370240b0f7c207cb26bd3702c649dc8dca
Deniz Savaskan <dalpsavaskan@Denizs-MacBook-Pro.local>
2018-11-30 22:44:42 +0300

    Initial Commit

---
+++ MyKittens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyKittens
+//
+//  Created by Deniz Alp Savaskan on 30.11.2018.
+//  Copyright © 2018 Deniz Alp Savaskan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
